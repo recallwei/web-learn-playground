fad937d0abb712a766d930f042d91681935f6e23
Bruce <recall4056@gmail.com>
2023-04-01 22:22:26 +0800

    feat: use rust directive

--- rust-tauri-webview/src/components/HelloWorld.vue
+++ rust-tauri-webview/src/components/HelloWorld.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { invoke } from '@tauri-apps/api'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+
+// invoke Rust directive
+invoke('greet', { name: 'World' }).then((res) => {
+  count.value = 250
+})
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button
+      type="button"
+      @click="count++"
+    >
+      count is {{ count }}
+    </button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -19,13 +29,19 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+    <a
+      href="https://vuejs.org/guide/quick-start.html#local"
+      target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
   </p>
   <p>
     Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    <a
+      href="https://github.com/vuejs/language-tools"
+      target="_blank"
+      >Volar</a
+    >
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
